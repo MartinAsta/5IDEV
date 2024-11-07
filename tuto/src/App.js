f78a3e17e453167ee4dd40f78e6d963bd7a0b369
Asta Martin <martin.asta@hotmail.com>
2024-11-07 20:29:10 +0100

    First commit

---
+++ tuto/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function MyButton() {
+  return (
+    <button>
+      Je suis un bouton
+    </button>
+  );
+}
+
+export default function MyApp() {
+  return (
+    <div>
+      <h1>Bienvenue dans mon appli</h1>
+      <MyButton />
+    </div>
+  );
+}
